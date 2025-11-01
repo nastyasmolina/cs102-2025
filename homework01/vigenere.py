823cef4ed4cdcec2ce817e7e61bf29ffda05f473
Anastasia Smolina <nastyasmolina00@mail.ru>
2025-11-01 12:28:21 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,6 +1,7 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,13 +10,32 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+    for i, char in enumerate(plaintext):
+        if char.isalpha():
+            key_char = keyword[i % key_length]
+            shift = ord(key_char) - ord("A")
+
+            if char.isupper():
+                base = ord("A")
+                encrypted_char = chr((ord(char) - base + shift) % 26 + base)
+            else:
+                base = ord("a")
+                encrypted_char = chr((ord(char) - base + shift) % 26 + base)
+
+            ciphertext += encrypted_char
+        else:
+
+            ciphertext += char
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -24,5 +44,22 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+
+    for i, char in enumerate(ciphertext):
+        if char.isalpha():
+            key_char = keyword[i % key_length]
+            shift = ord(key_char) - ord("A")
+
+            if char.isupper():
+                base = ord("A")
+                decrypted_char = chr((ord(char) - base - shift) % 26 + base)
+            else:
+                base = ord("a")
+                decrypted_char = chr((ord(char) - base - shift) % 26 + base)
+
+            plaintext += decrypted_char
+        else:
+            plaintext += char
     return plaintext
